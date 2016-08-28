f96a3b1bbaaf466437b5b961f2c071429851c33c
smalllamartin <smalllamartin@gmailcom>
2016-08-28 19:35:06 +0100

    WebView

---
+++ app/src/main/java/net/apkode/soa/MainActivity.java
@@ -0,0 +1,30 @@
+package net.apkode.soa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings websettings = webView.getSettings();
+
+        websettings.setJavaScriptEnabled(true);
+        websettings.setBuiltInZoomControls(true);
+        websettings.setDomStorageEnabled(true);
+
+        webView.loadUrl("https://hydrocarbure-transport.herokuapp.com");
+
+
+    }
+}
